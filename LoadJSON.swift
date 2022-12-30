a3669756c215b6c888feb27532a3a9cec9ac38a6
steady-on <whitebsm93@naver.com>
2022-12-30 15:10:12 +0900

    Comment
    - 주석 작성

--- LoadJSON.swift
+++ LoadJSON.swift
@@ -5,6 +5,7 @@
 import Foundation
 
 class LoadJSON {
+    // MARK: class 내에서 발생하는 에러처리
     enum LoadJSONError: Error, LocalizedError {
         case invalidURL
         
@@ -16,6 +17,7 @@ class LoadJSON {
         }
     }
     
+    // MARK: - 매개변수로 받아온 url에 있는 JSON을 구조체로 decoding해서 리턴하는 메서드
     func loadWebJSON<T: Decodable>(_ url: String) async throws -> T {
         guard let url = URL(string: url) else {
             print(LoadJSONError.invalidURL.errorDescription)
@@ -30,6 +32,7 @@ class LoadJSON {
         }
     }
 
+    // MARK: - 매개변수로 받아온 url에 있는 String 타입으로 리턴. 데이터 확인용 메서드
     func loadWebJSONtoString(_ url: String) async throws -> String {
         guard let url = URL(string: url) else {
             print(LoadJSONError.invalidURL.errorDescription)
